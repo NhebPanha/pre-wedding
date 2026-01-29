045faeaec39c0907c78e8f146bd9df7e4c81b3dc
unknown <hkmkh26@gmail.com>
2026-01-29 16:15:32 +0700

    update

--- app/components/home/Map.vue
+++ app/components/home/Map.vue
@@ -6,12 +6,11 @@
                 ទីតាំងកម្មវិធីរបស់យើង
                 <span class="absolute left-1/2 -bottom-2 w-16 h-1 bg-yellow-400 -translate-x-1/2 rounded"></span>
             </h2>
-
             <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 items-start">
                 <!-- Google Map -->
                 <div class="lg:col-span-2 rounded-2xl shadow-lg overflow-hidden bg-white">
                     <iframe class="w-full h-[360px]" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
-                        src="https:/www.google.com/maps?q=ចំបក់ នាងទើត តំបែរ ត្បូងឃ្មុំ&z=15&output=embed"
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d822.6589868120485!2d105.8776809384662!3d11.873486404263048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2skh!4v1769674364812!5m2!1sen!2skh"
                         allowfullscreen>
                     </iframe>
                 </div>
@@ -33,9 +32,7 @@
                                 </p>
                             </div>
                         </li>
-
                     </ul>
-
                     <!-- Button -->
                     <button
                         class="mt-10 w-full border-2 border-yellow-400 text-yellow-600 font-semibold py-3 rounded-xl hover:bg-yellow-400 hover:text-white transition">
--- app/components/layout/AppFooter.vue
+++ app/components/layout/AppFooter.vue
@@ -2,12 +2,10 @@
   <footer class="bg-[#0B1E3B] text-white pt-20 pb-6">
     <div class="max-w-4xl mx-auto text-center px-6">
       <div class="w-12 h-12 bg-gray-400/30 rounded-md mx-auto mb-10">
-         <span
-              class="material-symbols-outlined text-3xl md:text-4xl text-yellow-500"
-              style="font-variation-settings: 'FILL' 1"
-            >
-              photo_camera
-            </span>
+        <span class="material-symbols-outlined text-3xl md:text-4xl text-yellow-500"
+          style="font-variation-settings: 'FILL' 1">
+          photo_camera
+        </span>
       </div>
 
       <h2 class="text-3xl md:text-4xl font-black mb-6 leading-tight tracking-wide font-khmer">
@@ -15,21 +13,19 @@
       </h2>
 
       <p class="text-[#d4af37] text-base md:text-lg opacity-80 mb-12 max-w-2xl mx-auto leading-relaxed">
-        យើងមានការទទួលសក់ក្នុងចំនួនកំណត់សម្រាប់រដូវកាលខាងមុខនេះ។ ចូរយើងរួមគ្នាបង្កើតអនុស្សាវរីយ៍ដ៏អស្ចារ្យ។
+        យើងនៅទីនេះដើម្បីបង្កើតបទពិសោធន៍ពិសេសដែលអ្នកនិងអ្នកស្រលាញ់របស់អ្នកនឹងចងចាំជានិច្ច។
       </p>
 
       <div class="flex flex-col sm:flex-row gap-5 justify-center mb-24">
         <button
-          class="bg-[#EBC410] text-[#0B1E3B] font-black px-10 py-4 rounded-xl flex items-center justify-center gap-3 hover:bg-yellow-400 transition-all transform active:scale-95 shadow-lg shadow-yellow-500/10"
-        >
-          កក់ការថតរបស់អ្នក
+          class="bg-[#EBC410] text-[#0B1E3B] font-black px-10 py-4 rounded-xl flex items-center justify-center gap-3 hover:bg-yellow-400 transition-all transform active:scale-95 shadow-lg shadow-yellow-500/10">
+          មើលធថ្ងៃរៀបការ
           <span class="material-symbols-outlined text-xl">calendar_month</span>
         </button>
 
         <button
-          class="border-2 border-[#EBC410] text-[#EBC410] font-black px-10 py-4 rounded-xl flex items-center justify-center gap-3 hover:bg-[#EBC410]/10 transition-all transform active:scale-95"
-        >
-          សាកសួរតម្លៃសេវាកម្ម
+          class="border-2 border-[#EBC410] text-[#EBC410] font-black px-10 py-4 rounded-xl flex items-center justify-center gap-3 hover:bg-[#EBC410]/10 transition-all transform active:scale-95">
+          ទាក់ទងមកយើង
           <span class="material-symbols-outlined text-xl">mail</span>
         </button>
       </div>
@@ -38,23 +34,21 @@
     <div class="max-w-7xl mx-auto border-t border-white/10 mb-12"></div>
 
     <div class="max-w-7xl mx-auto px-8 flex flex-col md:flex-row justify-between items-center gap-8">
-      
+
       <div class="flex items-center gap-3 text-[#EBC410]">
         <div class="w-6 h-6  rounded-sm flex items-center justify-center">
-            <span
-              class="material-symbols-outlined text-3xl md:text-4xl text-yellow-500"
-              style="font-variation-settings: 'FILL' 1"
-            >
-              camera
-            </span>
+          <span class="material-symbols-outlined text-3xl md:text-4xl text-yellow-500"
+            style="font-variation-settings: 'FILL' 1">
+            camera
+          </span>
         </div>
-        <span class="uppercase font-black tracking-[0.2em] text-sm">ETERNAL STORY</span>
+        <span class="uppercase font-black tracking-[0.2em] text-sm">PANHA KHMER</span>
       </div>
 
       <nav class="flex flex-wrap justify-center gap-x-10 gap-y-4 text-sm font-medium text-white/80">
         <NuxtLink to="/" class="hover:text-[#EBC410] transition-colors">ទីតាំងដើម</NuxtLink>
         <NuxtLink to="/gallery" class="hover:text-[#EBC410] transition-colors">ការបង្ហាញរូបភាព</NuxtLink>
-        <NuxtLink to="/services" class="hover:text-[#EBC410] transition-colors">សេវាកម្ម</NuxtLink>
+        <NuxtLink to="/services" class="hover:text-[#EBC410] transition-colors">អតិថិជន</NuxtLink>
         <NuxtLink to="/contact" class="hover:text-[#EBC410] transition-colors">ទំនាក់ទំនង</NuxtLink>
       </nav>
 
